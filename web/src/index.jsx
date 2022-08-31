1c797c33147465d310382b1d9d8d77ea71f36fd7
CouncilDataProjectServiceAccount <councildataproject@gmail.com>
2022-08-31 00:41:54 +0000

    Initial commit

---
+++ web/src/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
+
+import "@councildataproject/cdp-frontend/dist/index.css";
+
+const config = {
+    firebaseConfig: {
+        options: {
+            projectId: "cdp-louisville-6fd32a38",
+        },
+        settings: {},
+    },
+    municipality: {
+        name: "Louisville",
+        timeZone: "America/Kentucky/Louisville",
+        footerLinksSections: [],
+    },
+}
+
+ReactDOM.render(
+    <div>
+        <AppConfigProvider appConfig={config}>
+            <App />
+        </AppConfigProvider>
+    </div>,
+    document.getElementById("root")
+);
